cf41999d8604ebf8ff81caad767e1350d2db082b
jorgeAtNesto <jorge@nesto.ca>
2022-02-22 12:49:01 -0500

    solved middleware issues

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,17 +1,17 @@
 import sqlite3
 import logging
 from flask import (
-    Flask, 
-    jsonify,  
-    render_template, 
-    request, 
-    url_for, 
-    redirect, 
+    Flask,
+    jsonify,
+    render_template,
+    request,
+    url_for,
+    redirect,
     flash,
 )
 #from itsdangerous import json
 from werkzeug.exceptions import abort
-from middleware import create_logger
+from middleware import AppLogger
 
 
 # Function to get a database connection.
@@ -41,55 +41,72 @@ def get_post_count(logger: logging.Logger) -> int:
 def get_post(post_id):
     connection = get_db_connection()
     post = connection.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
+                              (post_id,)).fetchone()
     connection.close()
     return post
 
+
 # Define the Flask application
+log = AppLogger(name="app", level=10)
+log.set_post_count(get_post_count(log.logger))
+
+
 def create_flask_instance() -> Flask:
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'your secret key'
-    app.logger = create_logger("app")
-    app.db_connection_count = 0
-    app.post_count = get_post_count(app.logger)
+    app.logger = log.logger
     return app
+
+
+def create_post(title: str, content: str):
+    connection = get_db_connection()
+    connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                       (title, content))
+    connection.commit()
+    connection.close()
 
 
 app = create_flask_instance()
 
 
-# Define the main route of the web application 
+# Define the main route of the web application
 @app.route('/')
+@log.increment_db_connection_count
 def index():
     connection = get_db_connection()
     posts = connection.execute('SELECT * FROM posts').fetchall()
     connection.close()
-    app.db_connection_count += 1
     return render_template('index.html', posts=posts)
 
-# Define how each individual article is rendered 
+# Define how each individual article is rendered
 # If the post ID is not found a 404 page is shown
+
+
 @app.route('/<int:post_id>')
+@log.increment_db_connection_count
 def post(post_id):
-    post = get_post(post_id)        
-    app.db_connection_count += 1
-    
+    post = get_post(post_id)
+
     if post is None:
         app.logger.info("A non-existing article is accessed.")
         return render_template('404.html'), 404
     else:
-      app.logger.info(f"An existing article is retrieved: {post['title']}")
-      return render_template('post.html', post=post)
+        app.logger.info(f"An existing article is retrieved: {post['title']}")
+        return render_template('post.html', post=post)
 
 # Define the About Us page
+
+
 @app.route('/about')
 def about():
     app.logger.info("The \"About Us\" page is retrieved")
     return render_template('about.html')
 
 
-# Define the post creation functionality 
+# Define the post creation functionality
 @app.route('/create', methods=('GET', 'POST'))
+@log.increment_db_connection_count
+@log.increment_post_count
 def create():
     if request.method == 'POST':
         title = request.form['title']
@@ -98,13 +115,7 @@ def create():
         if not title:
             flash('Title is required!')
         else:
-            connection = get_db_connection()
-            connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content))
-            app.db_connection_count += 1
-            connection.commit()
-            connection.close()
-            app.post_count += 1
+            create_post(title, content)
             app.logger.info(f"A new article is created: {title}")
             return redirect(url_for('index'))
 
@@ -118,13 +129,12 @@ def health_probe() -> dict:
 
 @app.route("/metrics")
 def metrics() -> dict:
-    app.db_connection_count += 1
     return jsonify({
-        "db_connection_count": app.db_connection_count,
-        "post_count": app.post_count
+        "db_connection_count": log.db_connection_count,
+        "post_count": log.post_count
     }), 200
 
 
 # start the application on port 3111
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111', debug=True)
+    app.run(host='0.0.0.0', port='3111', debug=True)
--- project/techtrends/middleware.py
+++ project/techtrends/middleware.py
@@ -1,15 +1,52 @@
-import logging 
+import logging
+from functools import wraps
+from dataclasses import (
+    dataclass,
+    field
+)
 
-def create_logger(name: str, level: int = 10) -> logging.Logger:
-    logger = logging.getLogger(name)
-    logger.setLevel(level)
-    
-    ch = logging.StreamHandler()
-    ch.setLevel(level)
 
-    formatter = logging.Formatter("%(levelname)s %(name)s %(asctime)s %(message)s")
-    ch.setFormatter(formatter)
+@dataclass
+class AppLogger:
+    name: str
+    level: int
+    post_count: int = 0
+    db_connection_count: int = 0
+    logger: logging.Logger = field(init=False)
 
-    logger.addHandler(ch)
+    def __post_init__(self):
+        self.logger = self.create_logger(self.name, self.level)
 
-    return logger
+    def create_logger(self, name: str, level: int = 10) -> logging.Logger:
+        logger = logging.getLogger(name)
+        logger.setLevel(level)
+
+        ch = logging.StreamHandler()
+        ch.setLevel(level)
+
+        formatter = logging.Formatter(
+            "%(levelname)s %(name)s %(asctime)s %(message)s"
+        )
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
+
+        return logger
+
+    def increment_db_connection_count(self, func):
+        @wraps(func)
+        def wrapped_func(*args, **kwargs):
+            res = func(*args, **kwargs)
+            self.db_connection_count += 1
+            return res
+        return wrapped_func
+
+    def increment_post_count(self, func):
+        @wraps(func)
+        def wrapped_func(*args, **kwargs):
+            res = func(*args, **kwargs)
+            self.post_count += 1
+            return res
+        return wrapped_func
+
+    def set_post_count(self, count: int):
+        self.post_count = count
